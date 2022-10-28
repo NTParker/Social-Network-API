3b09107cdb41bb3ac2121c5296e7def27723ddf3
Noah Parker <noahtmparker@gmail.com>
2022-10-28 12:05:11 -0400

    finished user and all api routes / added missing reaction stuff

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -84,6 +84,34 @@ const thoughtController = {
       })
       .catch((err) => res.status(400).json(err));
   },
+
+  // add reaction
+  addReaction({ params, body }, res) {
+    Thought.findOneAndUpdate(
+      { _id: params.id },
+      { $push: { reactions: body } },
+      { new: true }
+    )
+      .then((dbThoughtData) => {
+        if (!dbThoughtData) {
+          res.status(404).json({ message: 'No thought found with this id.' });
+          return;
+        }
+        res.json(dbThoughtData);
+      })
+      .catch((err) => res.status(400).json(err));
+  },
+
+  // delete reaction
+  deleteReaction({ params }, res) {
+    Thought.findOneAndUpdate(
+      { _id: params.id },
+      { $pull: { reactions: { reactionId: params.reactionId } } },
+      { new: true }
+    )
+      .then((dbThoughtData) => res.json(dbThoughtData))
+      .catch((err) => res.status(400).json(err));
+  },
 };
 
 module.exports = thoughtController;
--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -6,14 +6,18 @@ const {
   createThought,
   updateThought,
   deleteThought,
+  addReaction,
+  deleteReaction,
 } = require('../../controllers/thought-controller');
 
 router.route('/').get(getAllThoughts).post(createThought);
-
 router
   .route('/:id')
   .get(getThoughtById)
   .put(updateThought)
   .delete(deleteThought);
+
+router.route('/:id/reactions').post(addReaction);
+router.route('/:id/reactions/:reactionId').delete(deleteReaction);
 
 module.exports = router;
